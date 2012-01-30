bcf5f75ae53623990241d37888da7577093c008b
Tristan Brookes <tristan@tristan-brookes.co.uk>
2012-01-30 12:28:30 +0000

    Added ability to turn on or off jQuery in the module

--- helper.php
+++ helper.php
@@ -7,6 +7,25 @@ JModel::addIncludePath(JPATH_SITE.'/components/com_content/models');
 
 class mod_fancypantsaccordionHelper{
 	
+	public function load_jquery($params){
+		$doc = JFactory::getDocument();
+		$app = JFactory::getApplications();
+		
+		static $jqLoaded;
+		
+		if ($jqLoaded){
+			return;
+		}
+		
+		if ($params->get('load_jquery') && !$app->get('jQuery')){
+			$file=JURI::root(true).'/modules/mod_fancypantsaccordion/assets/js/jquery-1.7.1.min.js';
+			$app->set('jQuery',1);
+			$doc->addScript($file);
+			$doc->addScriptDeclaration("jQuery.noConflict();");
+			$jqLoaded = TRUE;
+		}
+	}
+	
 	public function getList($params){
 		$app = JFactory::getApplication();
 		//calls to application
